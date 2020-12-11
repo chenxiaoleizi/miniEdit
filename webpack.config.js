cd25ce7cb1cfbecd2fd38c89bca24feb0a85fc11
1 1 <chenlei>
2020-12-11 21:04:44 +0800

    handle css file

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,12 @@ module.exports = {
         rules: [
             {
                 test: /\.ts$/,
-                use: "ts-loader"
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
